3df734eb8314266bf86702f1f667615bff7fbc42
jud-as <guilhermeferreira082@gmail.com>
2024-12-16 12:51:31 -0300

    Created User Controller

--- src/main/java/com/jud_as/todosimple/models/User.java
+++ src/main/java/com/jud_as/todosimple/models/User.java
@@ -1,5 +1,6 @@
 package com.jud_as.todosimple.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
@@ -76,6 +77,7 @@ public class User {
         this.password = password;
     }
 
+    @JsonIgnore
     public List<Task> getTasks() {
         return tasks;
     }
