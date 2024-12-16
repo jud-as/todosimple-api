3914a5938fa6af2f0ae251c651ad821bf338df38
jud-as <guilhermeferreira082@gmail.com>
2024-12-16 15:17:51 -0300

    Created FindByUser_Id function

--- src/main/java/com/jud_as/todosimple/repositories/TaskRepository.java
+++ src/main/java/com/jud_as/todosimple/repositories/TaskRepository.java
@@ -1,15 +1,14 @@
 package com.jud_as.todosimple.repositories;
 
 import com.jud_as.todosimple.models.Task;
+import com.jud_as.todosimple.models.projection.TaskProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-
+    List<TaskProjection> findByUser_Id(Long userId);
 }
 
