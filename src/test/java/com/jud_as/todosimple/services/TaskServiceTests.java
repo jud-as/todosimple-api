1b863797a0d938b08455a4f1157b905036a860af
jud-as <guilhermeferreira082@gmail.com>
2024-12-18 16:51:15 -0300

    Resolving merge conflicts

--- src/test/java/com/jud_as/todosimple/services/TaskServiceTests.java
+++ src/test/java/com/jud_as/todosimple/services/TaskServiceTests.java
@@ -1,18 +1,12 @@
 package com.jud_as.todosimple.services;
-<<<<<<< HEAD
 import com.jud_as.todosimple.controllers.TaskController;
 import com.jud_as.todosimple.models.Task;
 import com.jud_as.todosimple.models.User;
 import com.jud_as.todosimple.repositories.TaskRepository;
 import com.jud_as.todosimple.repositories.UserRepository;
-import jakarta.transaction.TransactionScoped;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
-=======
 import com.jud_as.todosimple.BaseTestSetup;
-import com.jud_as.todosimple.models.Task;
-import jakarta.transaction.Transactional;
->>>>>>> 742a8b7a1e80e5099af1faadcf49d511469f340a
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,16 +15,11 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 @SpringBootTest
-<<<<<<< HEAD
-public class TaskServiceTests {
-=======
 public class TaskServiceTests extends BaseTestSetup {
->>>>>>> 742a8b7a1e80e5099af1faadcf49d511469f340a
 
     @Autowired
     TaskService taskService;
 
-<<<<<<< HEAD
     @Autowired
     TaskRepository taskRepository;
 
@@ -51,8 +40,6 @@ public class TaskServiceTests extends BaseTestSetup {
         taskRepository.save(task);
     }
 
-=======
->>>>>>> 742a8b7a1e80e5099af1faadcf49d511469f340a
     @Test
     @Transactional
     void testFindById() {
