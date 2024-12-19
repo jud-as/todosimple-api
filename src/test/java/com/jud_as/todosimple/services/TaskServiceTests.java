f9e39ce72474dd72d471965541bed0c615ca874d
jud-as <guilhermeferreira082@gmail.com>
2024-12-18 21:55:39 -0300

    Fixing UserServiceTask redundancy

--- src/test/java/com/jud_as/todosimple/services/TaskServiceTests.java
+++ src/test/java/com/jud_as/todosimple/services/TaskServiceTests.java
@@ -1,11 +1,6 @@
 package com.jud_as.todosimple.services;
-import com.jud_as.todosimple.controllers.TaskController;
 import com.jud_as.todosimple.models.Task;
-import com.jud_as.todosimple.models.User;
 import com.jud_as.todosimple.repositories.TaskRepository;
-import com.jud_as.todosimple.repositories.UserRepository;
-import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.BeforeEach;
 import com.jud_as.todosimple.BaseTestSetup;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,25 +18,7 @@ public class TaskServiceTests extends BaseTestSetup {
     @Autowired
     TaskRepository taskRepository;
 
-    User testUser;
-    @Autowired
-    UserRepository userRepository;
-
-    @BeforeEach
-    void setUp() {
-        testUser = new User();
-        testUser.setUsername("testUser");
-        testUser.setPassword("testPassword");
-        userRepository.save(testUser);
-
-        Task task = new Task();
-        task.setUser(testUser);
-        task.setDescription("Test Task");
-        taskRepository.save(task);
-    }
-
     @Test
-    @Transactional
     void testFindById() {
         Task task = taskRepository.findAll().get(0);
         assertNotNull(taskService.findById(task.getId()));
